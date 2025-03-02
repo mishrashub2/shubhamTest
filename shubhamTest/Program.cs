6901f66518952baef5eb2f3b8daaa0cb9bdca012
Shubham <mishrashub2@gmail.com>
2025-03-02 19:25:59 +0530

    Add project files.

---
+++ shubhamTest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace shubhamTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Shhubham is hard working man");
+            Console.WriteLine("_______________________________");
+            Console.ReadLine();
+        }
+    }
+}
